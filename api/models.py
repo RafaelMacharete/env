a8b8067852c6b02ef34735cd8124102b4b10745f
Rafael da Silva Macharete <52752592809@>
2024-09-20 14:45:36 -0300

    20.09.2024 14:45h

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
 # Create your models here.
+
+class Produto(models.Model):
+    codigoProduto = models.CharField(max_length=255) # Campo de char, máximo de 255
+    tituloProduto = models.CharField(max_length=255) # Campo de char, máximo de 255
+    preco = models.DecimalField(max_digits=10, decimal_places=2) # Campo de decimal (float), máximo de digitos de 10 e de decimal é 2
+    descricao = models.TextField(max_length=255) # Campo de char, máximo de 255
+    imagemProduto = models.CharField(max_length=255) # Campo de char, máximo de 255
+    categoriaProduto = models.CharField(max_length=255) # Campo de char, máximo de 255
+    classProduto = models.DecimalField(max_digits=2, decimal_places=1) # Campo de decimal (float), máximo de digitos de 10 e de decimal é 2
+    exibirHome = models.BooleanField(default=True) #Campo booleano, padrão dele é True (verdadeiro)
+
