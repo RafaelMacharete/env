f163e647ff8f1774d9ace9d4866d3081267af607
Rafael da Silva Macharete <52752592809@>
2024-09-27 14:37:16 -0300

    27.09.2024-14.37

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Produto
+from .serializers import ProdutoSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def listarProdutos(request):
+    if request.method == "GET":
+        queryset = Produto.objects.all() # seleciona todos os produtos
+        serializer = ProdutoSerializer(queryset, many=True) # transforma em JSON
+        return Response(serializer.data)
+    elif request.method == 'POST': 
+        serializer = ProdutoSerializer(data = request.data)
+        serializer.is_valid()
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
